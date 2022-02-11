ecc0279c61342b645bc2dc3f0abea8364109b9fc
Frederic Kneier <frederic@kneier.net>
2022-02-11 20:59:02 +0100

    Adds implementation for challenge

--- src/main/kotlin/de/solugo/rpsgame/model/Game.kt
+++ src/main/kotlin/de/solugo/rpsgame/model/Game.kt
@@ -2,6 +2,22 @@ package de.solugo.rpsgame.model
 
 object Game {
 
-    fun challenge(player: Shape, opponent: Shape): Outcome = TODO()
+    fun challenge(player: Shape, opponent: Shape): Outcome = when (player) {
+        Shape.Rock -> when (opponent) {
+            Shape.Rock -> Outcome.Draw
+            Shape.Paper -> Outcome.Loss
+            Shape.Scissors -> Outcome.Win
+        }
+        Shape.Scissors -> when (opponent) {
+            Shape.Rock -> Outcome.Loss
+            Shape.Paper -> Outcome.Win
+            Shape.Scissors -> Outcome.Draw
+        }
+        Shape.Paper -> when (opponent) {
+            Shape.Rock -> Outcome.Win
+            Shape.Paper -> Outcome.Draw
+            Shape.Scissors -> Outcome.Loss
+        }
+    }
 
 }
