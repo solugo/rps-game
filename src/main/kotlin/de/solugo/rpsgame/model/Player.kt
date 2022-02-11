f9e998c194e94b85cdd970b9abdca8987247b2de
Frederic Kneier <frederic@kneier.net>
2022-02-11 21:51:26 +0100

    Implements player decisions

--- src/main/kotlin/de/solugo/rpsgame/model/Player.kt
+++ src/main/kotlin/de/solugo/rpsgame/model/Player.kt
@@ -2,7 +2,7 @@ package de.solugo.rpsgame.model
 
 class Player(val decision: () -> Shape) {
     companion object {
-        val DECISION_ROCK = { TODO() }
-        val DECISION_RANDOM = { TODO() }
+        val DECISION_ROCK = { Shape.Rock }
+        val DECISION_RANDOM = { Shape.values().random() }
     }
 }
